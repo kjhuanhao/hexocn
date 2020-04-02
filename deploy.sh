786e52cdde11d15e832ece4f70c3715c236514e7
huanhao <2077780946@qq.com>
2020-04-02 17:09:24 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,28 @@
 set -e
 
 # build
-#npm run build
+npm run build
 
 # navigate into the build output directory
-#cd public
+cd public
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+#echo 'hexocn.cn' > CNAME
 
-#git init
+git init
 git add -A
 git commit -m 'deploy'
 
 # please change it to your username
 
 #git@github.com:<username>/hexocn.git
-#git remote add origin git@github.com:kjhuanhao/hexocn.git
-
+#git remote add origin git@e.coding.net:huanhao/hexocn.git
+#git@e.coding.net:huanhao/hexocn.git
+#git remote rm origin
+git pull origin master 
+git remote add origin git@github.com:kjhuanhao/hexocnwebsite.git
 # push
-#git push --set-upstream origin master
-git push
+git push --set-upstream origin master
+#git push
 
-#cd -
+cd -
