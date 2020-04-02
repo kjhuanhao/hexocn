b0215fe21044e42fd83f1ef49d48f46ade8c5991
huanhao <2077780946@qq.com>
2020-04-02 23:17:54 +0800

    update

---
+++ .github/ISSUE_TEMPLATE/docs.md
@@ -0,0 +1,13 @@
+---
+name: 贡献文档
+about: 贡献你编写的任何文档，只要关于hexo即可
+title: '【提交文档】'
+labels: 'documentation'
+assignees: ''
+
+---
+
+## 标题
+
+
+
--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd public
 
 # if you are deploying to a custom domain
-#echo 'hexocn.cn' > CNAME
+echo 'hexocn.cn' > CNAME
 
 git init
 git add -A
@@ -19,11 +19,25 @@ git commit -m 'deploy'
 # please change it to your username
 
 #git@github.com:<username>/hexocn.git
-#git remote add origin git@e.coding.net:huanhao/hexocn.git
+
+#git remote add origin git@e.coding.net:huanhao/hexocnwebsite.git
+#git pull origin master --allow-unrelated-histories
+#git push --set-upstream origin master
+#git push --set-upstream origin master
 #git@e.coding.net:huanhao/hexocn.git
 #git remote rm origin
-git pull origin master 
-git remote add origin git@github.com:kjhuanhao/hexocnwebsite.git
+#cd ~/Desktop/hexocn/
+#npm run build
+#cd public
+#echo 'hexocn.cn' > CNAME
+
+#git init
+#git add -A
+#git commit -m 'deploy'
+
+git remote add origin git@github.com:mrhuanhao/mrhuanhao.github.io.git
+
+#git pull origin master --allow-unrelated-histories
 # push
 git push --set-upstream origin master
 #git push
