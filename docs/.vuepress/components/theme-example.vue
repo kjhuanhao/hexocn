bf52b7e508de7eba435415528d29f519265bf605
kali <65162386+kali65536@users.noreply.github.com>
2020-07-24 10:20:49 +0800

    Update theme-example.vue
    name: ‘kali’,
    desc: ‘遇见更好的自己’,
    link: ‘https://kali.js.org/',
    themeimg: ‘https://cdn.jsdelivr.net/gh/kali65536/PicGo@latest/2020/07/20/48d173c0859c68371c75d738374a951a.png'

--- docs/.vuepress/components/theme-example.vue
+++ docs/.vuepress/components/theme-example.vue
@@ -91,6 +91,12 @@ export default {
        link: 'https://chuchendjs.github.io',
        themeimg: 'https://cdn.jsdelivr.net/gh/chuchendjs/picgo/picgo/vuh.png'
        }
+       {
+       name: ‘kali’,
+       desc: ‘遇见更好的自己’,
+       link: ‘https://kali.js.org/',
+       themeimg: ‘https://cdn.jsdelivr.net/gh/kali65536/PicGo@latest/2020/07/20/48d173c0859c68371c75d738374a951a.png'
+       }
       ]
     }
   }
